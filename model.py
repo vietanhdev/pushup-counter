d89bc0adfe66b4b8318e29a83d29a1d316190435
Viet Anh Nguyen <vietanh.dev@gmail.com>
2020-11-11 14:22:58 +0700

    Clean up

--- model.py
+++ model.py
@@ -10,27 +10,6 @@ from tensorflow.keras.applications import MobileNetV2
 
 def build_model(seq_len=10):
 
-    # model=Sequential()
-    # model.add(TimeDistributed(Conv2D(32, (3, 3), padding='same'), input_shape=(seq_len, 224, 224, 3)))
-    # model.add(TimeDistributed(Activation('relu')))
-    # model.add(TimeDistributed(Dropout(0.25)))
-    # model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
-    # model.add(TimeDistributed(Conv2D(64, (3, 3))))
-    # model.add(TimeDistributed(Activation('relu')))
-    # model.add(TimeDistributed(Dropout(0.25)))
-    # model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
-    # model.add(TimeDistributed(Conv2D(128, (3, 3))))
-    # model.add(TimeDistributed(Activation('relu')))
-    # model.add(TimeDistributed(Dropout(0.25)))
-    # model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2))))
-    # model.add(TimeDistributed(Dropout(0.5)))
-    # model.add(TimeDistributed(Flatten()))
-    # model.add(TimeDistributed(Dense(512)))
-    # model.add(TimeDistributed(Dropout(0.25)))
-    # model.add(TimeDistributed(Dense(35)))
-    # model.add(LSTM(128, return_sequences=True))
-    # model.add(TimeDistributed(Dense(1, activation = "sigmoid")))
-
     model = Sequential()    
     model.add(TimeDistributed(MobileNetV2(weights='imagenet', include_top=False), input_shape=(seq_len, 224, 224, 3)))
     model.add(TimeDistributed(GlobalAveragePooling2D()))
--- test_video.py
+++ test_video.py
@@ -1,18 +1,9 @@
 from tensorflow.keras.models import load_model
-import tensorflow as tf
-from tensorflow.keras import backend as K
+from losses import focal_loss
 import cv2
 import numpy as np
 
 VIDEO = "/mnt/DATA/PUSHUP_PROJECT/processed/270.mp4"
-
-
-def focal_loss(gamma=2., alpha=.25):
-	def focal_loss_fixed(y_true, y_pred):
-		pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-		pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-		return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
-	return focal_loss_fixed
 
 model = load_model("model.43.h5", custom_objects={"focal_loss_fixed": focal_loss})
 
